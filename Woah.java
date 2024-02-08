a0b7ec58f69b695e6b01c28e09df747ffb66c86e
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-02-08 21:54:09 +0000

    Init commit

---
+++ Woah.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Woah {
+
+  // Linter being tested
+
+  public static void main(String[] args) {
+    System.out.println("Hello, Spencer!");
+    System.out.println("Look at this funny face: ( ͡° ʖ ͡°)");
+  }
+}
